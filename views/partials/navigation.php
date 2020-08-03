d7700ba496c0e665bfc9781896691af37484fd3f
v.lelicanin <v.lelicanin@sae.edu>
2020-08-03 17:17:49 +0200

    Posts

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -4,5 +4,7 @@
         <li><a href="/contact">Contact</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/about/culture">About Our Culture</a></li>
+        <li><a href="/posts">Posts</a></li>
+        <li><a href="/users">Users</a></li>
     </ul>
 </nav>
